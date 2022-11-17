8a272c9e63d78af3b41b1391678050f86b728a63
Dmytro Tkachenko <d.tkachenko@spd-ukraine.com>
2022-11-17 10:41:33 +0200

    create spiner template

---
+++ src/components/spiner/spiner.template.ts
@@ -0,0 +1,7 @@
+export const spinerTemplate = () => {
+  return `
+    <div class="spinner-border text-info" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  `;
+};
