77243b265c293f6522817c7fcca99852927b53be
Dmytro Tkachenko <d.tkachenko@spd-ukraine.com>
2022-11-21 16:32:07 +0200

    chenge order

--- src/widgetsUrlData.mock.ts
+++ src/widgetsUrlData.mock.ts
@@ -2,19 +2,19 @@ import { IWidgetsUrlData } from "./utils/runWidgetsInSequence/runWidgetsInSequen
 
 export const widgetsUrlData: IWidgetsUrlData[] = [
   {
-    title: 'currencyWidget',
-    url: 'https://economia.awesomeapi.com.br/json/last/USD-UAH,EUR-UAH,BTC-USD',
-  },
-  {
-    title: 'newsWidget',
-    url: 'https://inshorts.deta.dev/news?category=science',
+    title: 'weatherWidget',
+    url: 'https://api.openweathermap.org/data/2.5/weather?q=Cherkasy&units=metric&appid=f8abe57730e73adeaf76486b28bcee9f',
   },
   {
     title: 'memesWidget',
     url: 'https://api.imgflip.com/get_memes'
   },
   {
-    title: 'weatherWidget',
-    url: 'https://api.openweathermap.org/data/2.5/weather?q=Cherkasy&units=metric&appid=f8abe57730e73adeaf76486b28bcee9f',
+    title: 'newsWidget',
+    url: 'https://inshorts.deta.dev/news?category=science',
+  },
+  {
+    title: 'currencyWidget',
+    url: 'https://economia.awesomeapi.com.br/json/last/USD-UAH,EUR-UAH,BTC-USD',
   },
 ];
